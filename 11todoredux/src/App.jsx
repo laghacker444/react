0a216aa7fa44e50349a59956b2b656e50c53deca
laghacker444 <l.a.g.hack444@gmail.com>
2024-09-18 18:35:36 +0530

    todo project by redux tool using state and action make easy by doing same  todo in context api

---
+++ 11todoredux/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-3xl text-center text-black p-3 m-4'>Hello</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
